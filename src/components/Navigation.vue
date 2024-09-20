29b4efe59693bbcfe373aa7a0ba64e5de53a204e
przemek <pastuszka.przemyslaw@gmail.com>
2024-09-20 22:53:43 +0200

    make it better

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,10 +2,10 @@
   <div class="bg-white py-5 sticky top-0 w-full shadow-md z-50">
     <div class="container mx-auto flex justify-between items-center p-2 max-w-4xl">
       <div class="space-x-2 flex flex-row">
-        <img src="/icon.jpg" alt="icon" class="w-8 h-8 object-contain">
+        <img src="/icon.jpg" alt="icon" class="hidden md:block w-8 h-8 object-contain">
       <h1 class="text-2xl font-bold">Przemek Pastuszka</h1>
       </div>
-      <div class="space-x-6 flex flex-row">
+      <div class="space-x-4 flex flex-row">
         <a href="https://www.linkedin.com/in/ppastuszka/" rel="noopener noreferrer" target="_blank"><img src="/linkedin.svg" alt="linkedin" class="w-8 h-8 object-contain"></a>
         <a href="https://github.com/przemekpastuszka" rel="noopener noreferrer" target="_blank"><img src="/github-mark.png" alt="github" class="w-8 h-8 object-contain"></a>
       </div>
